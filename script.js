6cb135de12911a0496cc39050a872ebe6f6effaf
Андрей Соловьев <gmail.com>
2025-02-15 15:42:24 +0700

    add base code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('server is work a');
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+// Обработчик главной страницы
+app.get('/', (req, res) => {
+	res.send('Сервер работает!')
+})
+
+// Запуск сервера
+app.listen(PORT, () => {
+	console.log(`Сервер запущен на http://localhost:${PORT}`)
+})
